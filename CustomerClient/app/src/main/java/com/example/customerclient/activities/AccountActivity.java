9878ba4c373d715833945207f1450d2d41b8414a
Unas <unassikandar@gmail.com>
2019-03-11 21:29:05 +0300

    change dir

--- CustomerClient/app/src/main/java/com/example/customerclient/activities/AccountActivity.java
+++ CustomerClient/app/src/main/java/com/example/customerclient/activities/AccountActivity.java
@@ -1,4 +1,4 @@
-package com.example.customerclient;
+package com.example.customerclient.activities;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -14,10 +14,9 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.example.customerclient.R;
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
--- CustomerClient/app/src/main/java/com/example/customerclient/activities/SignUp.java
+++ CustomerClient/app/src/main/java/com/example/customerclient/activities/SignUp.java
@@ -1,4 +1,4 @@
-package com.example.customerclient;
+package com.example.customerclient.activities;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
@@ -10,6 +10,7 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.example.customerclient.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
