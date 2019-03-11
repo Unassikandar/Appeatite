b6b3cf17fda5d7a1e5b79417d3a27727d5e95c6f
Faaiz Haque <32203451+FaaizHaque@users.noreply.github.com>
2019-03-11 18:33:03 +0300

    Fixed QR Scanner

--- CustomerClient/app/src/main/java/com/example/customerclient/MainActivity.java
+++ CustomerClient/app/src/main/java/com/example/customerclient/MainActivity.java
@@ -15,6 +15,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.varvet.barcodereadersample.QRScanner;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
