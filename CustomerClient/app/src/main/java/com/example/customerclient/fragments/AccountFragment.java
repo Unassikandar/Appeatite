ea2520d6e69d5f7e0428e360cf2fd9741e6b19fa
Faaiz Haque <32203451+FaaizHaque@users.noreply.github.com>
2019-04-09 22:12:00 +0300

    Account fragment updated

--- CustomerClient/app/src/main/java/com/example/customerclient/fragments/AccountFragment.java
+++ CustomerClient/app/src/main/java/com/example/customerclient/fragments/AccountFragment.java
@@ -7,13 +7,34 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.customerclient.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static java.sql.DriverManager.println;
 
 public class AccountFragment extends Fragment {
     @Nullable
+
+
+    private FirebaseAuth mAuth;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
         return inflater.inflate(R.layout.fragment_account, container, false);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        TextView tv = (TextView) getView().findViewById(R.id.textview0);
+        String temp = currentUser.getEmail();
+        tv.append(temp);
     }
 }
--- CustomerClient/app/src/main/java/com/example/customerclient/fragments/SettingsFragment.java
+++ CustomerClient/app/src/main/java/com/example/customerclient/fragments/SettingsFragment.java
@@ -7,13 +7,41 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.example.customerclient.R;
 
 public class SettingsFragment extends Fragment {
+
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+    TextView textView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        /*View view =  inflater.inflate(R.layout.fragment_settings, container, false);
+        radioGroup = view.findViewById(R.id.radioGroup);
+        Button buttonApply = view.findViewById(R.id.button3);
+        buttonApply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });*/
+
         return inflater.inflate(R.layout.fragment_settings, container, false);
     }
+
+    public void checkButton(View v)
+    {
+        int radioId = radioGroup.getCheckedRadioButtonId();
+        radioButton = v.findViewById(radioId);
+    }
+
+
 }
